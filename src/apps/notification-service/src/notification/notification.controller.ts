bd08f108ab111718fcb4ac040bdebc31b8a97094
AnNguyen <theesan2k1@gmail.com>
2025-05-22 21:47:35 +0700

    feat: create route update read notify

--- src/apps/notification-service/src/notification/notification.controller.ts
+++ src/apps/notification-service/src/notification/notification.controller.ts
@@ -1,9 +1,8 @@
 // review.controller.ts
 import { NotificationService } from './notification.service';
-import { ApiTags, } from '@nestjs/swagger';
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, NotFoundException, Param, Post, Query } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
-
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Notification')
 @Controller('Notification')
@@ -26,5 +25,32 @@ export class NotificationController {
   @Get()
   async getUnreadNotifications(@Query('userId') userId: string) {
     return this.notificationService.getUnreadByUser(userId);
+  }
+
+
+  @Post(':id/read')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Đánh dấu thông báo đã đọc' })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'ID của thông báo cần đánh dấu là đã đọc',
+    type: String,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Thông báo đã được đánh dấu là đã đọc',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Không tìm thấy thông báo',
+  })
+  async markAsRead(@Param('id') id: string) {
+    const updated = await this.notificationService.markAsRead(id);
+    if (!updated) {
+      // Trả về 404 nếu không tìm thấy
+      throw new NotFoundException(`Notification with ID ${id} not found`);
+    }
+    return updated;
   }
 }
